47e4f4e60ffcf7d9515a9e63074b426ead55fa3d
Midka <me@midka.dev>
2024-01-21 14:19:32 +0000

    feat(lastfm-widget): add title

--- src/lib/components/LastFMWidget.svelte
+++ src/lib/components/LastFMWidget.svelte
@@ -10,30 +10,33 @@
 </script>
 
 <div aria-label="last.fm recently listened to" class="container">
-	<img src={track.image[2].uri} alt={`Cover image for ${track.name}`} />
-	<div aria-label="info about track" class="info">
-		<div aria-label="name and artist" class="data">
-			<p aria-label="track name">{track.name}</p>
-			<p aria-label="artist name">By {track.artist.name}</p>
-			<p aria-label="album name">Album: {track.album.name}</p>
+	<h2>Recently listened to</h2>
+	<div class="content-container">
+		<img src={track.image[2].uri} alt={`Cover image for ${track.name}`} />
+		<div aria-label="info about track" class="info">
+			<div aria-label="name and artist" class="data">
+				<p aria-label="track name">{track.name}</p>
+				<p aria-label="artist name">By {track.artist.name}</p>
+				<p aria-label="album name">Album: {track.album.name}</p>
+			</div>
+			<div aria-label="actions" class="actions">
+				<a href={track.url} target="_blank"
+					>View Track
+					<ExternalLinkIcon />
+				</a>
+				<a href={`https://last.fm/user/${env.PUBLIC_LASTFM_USER}`} target="_blank"
+					>Last.FM profile
+					<ExternalLinkIcon />
+				</a>
+			</div>
 		</div>
-		<div aria-label="actions" class="actions">
-			<a href={track.url} target="_blank"
-				>View Track
-				<ExternalLinkIcon />
-			</a>
-			<a href={`https://last.fm/user/${env.PUBLIC_LASTFM_USER}`} target="_blank"
-				>Last.FM profile
-				<ExternalLinkIcon />
-			</a>
+		<div class="info-icon">
+			<InfoSquareIcon
+				label="Last.FM Widget"
+				description="shows my recently listened to track & info about it"
+				on:click={() => alert('clicked')}
+			/>
 		</div>
-	</div>
-	<div class="info-icon">
-		<InfoSquareIcon
-			label="Last.FM Widget"
-			description="shows my recently listened to track & info about it"
-			on:click={() => alert('clicked')}
-		/>
 	</div>
 </div>
 
@@ -54,9 +57,19 @@
 		padding: 0.5rem 0.75rem;
 
 		display: flex;
-		gap: 0.75rem;
+		flex-direction: column;
+		gap: 0.5rem;
+		padding: 1rem;
+
+		> h2 {
+			font-size: 2rem;
+		}
+	}
+
+	.content-container {
+		display: flex;
 		align-items: center;
-		height: 9rem;
+		gap: 0.75rem;
 	}
 
 	.info {
@@ -119,6 +132,16 @@
 		position: absolute;
 		top: 0.5rem;
 		right: 0.5rem;
-		--size: 2rem;
+		--size: 2.25rem;
+
+		cursor: pointer;
+
+		&:hover,
+		&:focus,
+		&:focus-within {
+			color: $primary;
+		}
+
+		transition: all 0.2s ease-in-out;
 	}
 </style>
