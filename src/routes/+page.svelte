fb937ccfdc4d843530b0516534b6309b68290bf5
Midka <me@midka.dev>
2024-05-20 20:29:08 +0000

    feat: cool button animation
    https://codepen.io/pizza3/pen/qmerBv

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,9 +10,27 @@
 
 <!-- <LastFmWidget tracks={data.props.tracks} /> -->
 
-<h1>Welcome!</h1>
-<h2>I'm Midka. The creator of this site.</h2>
 <section class="general">
+	<h1>Welcome!</h1>
+	<h2>I'm Midka. The creator of this site.</h2>
+	<div class="navigation-buttons">
+		<div class="button-wrapper">
+			<span>About me</span>
+			<button>About me</button>
+		</div>
+		<div class="button-wrapper">
+			<span>Blog</span>
+			<button>Blog</button>
+		</div>
+		<div class="button-wrapper">
+			<span>Projects</span>
+			<button>Projects</button>
+		</div>
+		<div class="button-wrapper">
+			<span>Contact</span>
+			<button>Contact</button>
+		</div>
+	</div>
 	<!-- <p>teen who likes to help others</p> -->
 	<!-- <a href="https://www.google.com/maps/place/Helsinki"> @ Helsinki, Finland </a> -->
 </section>
@@ -27,3 +45,25 @@
 	<h2>Last FM</h2>
 	<JsonViewer jsonData={data} />
 </section> -->
+
+<style lang="scss">
+	section.general {
+		margin: 1rem 0;
+		gap: 2rem;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.navigation-buttons {
+		display: flex;
+		width: 100%;
+		gap: 1rem;
+		justify-content: space-between;
+
+		> button {
+			font-size: 2em;
+		}
+	}
+</style>
