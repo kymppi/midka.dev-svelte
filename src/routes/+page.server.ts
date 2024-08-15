dc371d141328b2c0c762b7741753b091ce47c30d
Midka <me@midka.dev>
2024-08-15 12:26:13 +0300

    remove: WF

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,21 +1,21 @@
 import { env } from '$env/dynamic/private';
 import { env as pubEnv } from '$env/dynamic/public';
 import { LastFMClient } from '$lib/lastfm.client';
-import { WriteFreelyClient } from '$lib/writefreely.client';
+//import { WriteFreelyClient } from '$lib/writefreely.client';
 
 export async function load() {
 	const apiClient = new LastFMClient(env.LASTFM_API_KEY || '', pubEnv.PUBLIC_LASTFM_USER || '');
 
 	const tracks = await apiClient.getRecentTracks(2);
 
-	const wfClient = new WriteFreelyClient(env.WF_INSTANCE || '', env.WF_COLLECTION || '');
+	//const wfClient = new WriteFreelyClient(env.WF_INSTANCE || '', env.WF_COLLECTION || '');
 
-	const posts = await wfClient.getPosts();
+	//const posts = await wfClient.getPosts();
 
 	return {
 		props: {
-			tracks,
-			posts
+			tracks
+			//posts
 		}
 	};
 }
